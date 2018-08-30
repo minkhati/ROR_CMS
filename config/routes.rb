c92d79b789e92ae442e62927a310b9526ac67f93
Min Khati <meetup.min@gmail.com>
2018-08-30 16:44:57 -0400

    Add Subjects controller and resourceful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,24 @@
 Rails.application.routes.draw do
+  
+  root 'demo#index'
+
+  # delete is not included in resources by default
+  resources :subjects do
+    member do
+      get :delete
+    end
+
+    # collection do
+    #   get :export
+    # end
+
+  end
+
   get 'demo/index'
   get 'demo/hello'
   get 'demo/otherhello'
   get 'demo/lynda'
-  root 'demo#index'
-
+  
   # Default route
   # get ':controller(/:action/(:id))'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
