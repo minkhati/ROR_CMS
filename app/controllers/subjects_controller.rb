237b342f220fcf09a2ba4886573786869f5ab942
Min Khati <meetup.min@gmail.com>
2018-08-31 13:26:00 -0400

    Implement CRUD for Subject Controller

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,25 +1,66 @@
 class SubjectsController < ApplicationController
   def index
+    @subjects = Subject.sorted
   end
 
   def show
+    @subject = Subject.find(params[:id])
   end
 
   def new
+    @subject = Subject.new
   end
 
   def create
+    # Instantiate a new object using form parameters
+    @subject = Subject.new(subject_params)
+    
+    # Save the object
+    if @subject.save
+      # If save successeds, redirect to the index action
+      flash[:notice] = "Subject created successfully."
+      redirect_to subjects_path
+    else
+      # If save fails, redisplays the form so user can fix problems
+      render 'new'
+    end
   end
 
   def edit
+    @subject = Subject.find(params[:id])
   end
 
   def update
+    # Find a object using form parameters
+    @subject = Subject.find(params[:id])
+    
+    # Update the object attributes
+    if @subject.update_attributes(subject_params)
+      # If update successeds, redirect to the show action
+      flash[:notice] = "Subject updated successfully."
+      redirect_to subject_path(@subject)
+    else
+      # If update fails, redisplays the form so user can fix problems
+      render 'edit'
+    end
   end
 
   def delete
+    # Find a object using form parameters
+    @subject = Subject.find(params[:id])
   end
 
   def destroy
+    @subject = Subject.find(params[:id])
+    @subject.destroy
+    flash[:notice] = "Subject '#{@subject.name}' deleted successfully."
+    redirect_to subjects_path
   end
+
+  private 
+  
+    def subject_params
+      params.require(:subject).permit(:name, :position, :visible)
+    end
+
 end
